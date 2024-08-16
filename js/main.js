a7ad3c1f0ae81d9b7fea0164af1abebcd48cb6c4
Torro15 <svetlgrish28@gmail.com>
2024-08-16 20:26:10 +0300

    js edit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 
-const images = document.querySelectorAll('.achievements__grid-mobile .slide');
-let curIndex = 0;
+window.addEventListener('load', function() {
+    var plane = document.querySelector(".achievements__grid-img");
+    plane.classList.add("plane-vis");
+});
+
+//Карусель для карточек
+
+const images = document.querySelector('.achievements__grid-mobile').querySelectorAll('.achievements__grid-item');
+let i= 0;
 
 function showImage(index) {
-    images[curIndex].classList.remove('active');
+    images[i].classList.remove('active');
     images[index].classList.add('active');
-    curIndex = index;
+    i = index;
 }
 
-document
-    .querySelector('.achievements__grid-mobile.controlls')
-    .addEventListener('click', function (event) {
+document.querySelector('.achievements__grid-mobile').addEventListener('click', function (event) {
         if (event.target.classList.contains('prev-button')) {
-            let index = curIndex - 1;
+            let index = i- 1;
             if (index < 0) {
                 index = images.length - 1;
             }
             showImage(index);
         } else if (event.target.classList.contains('next-button')) {
-            let index = curIndex + 1;
+            let index = i + 1;
             if (index >= images.length) {
                 index = 0;
             }
@@ -26,4 +31,5 @@ document
         }
     });
 
-showImage(curIndex);
+showImage(i);
+
