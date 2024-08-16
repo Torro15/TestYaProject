f69e2aef1b2ac475552bf6f0671490f87e4f5dde
Torro15 <svetlgrish28@gmail.com>
2024-08-16 15:10:26 +0300

    changes to js

--- js/main.js
+++ js/main.js
@@ -1,52 +1,51 @@
 
-// const slides = document.querySelectorAll('.team__box-item');
-// const totalSlides = slides.length;
-// let currentIndex = 0;
+const slides = document.querySelectorAll('.team__box-item');
+const totalSlides = slides.length;
+let currentIndex = 0;
 
-// const updateIndicator = () => {
-//     const indicator = document.querySelector('.count');
-//     indicator.innerText = `${currentIndex + 1}/6`;
-// };
+const updateIndicator = () => {
+    const indicator = document.querySelector('.count');
+    indicator.innerText = `${currentIndex + 1}/6`;
+};
 
-// const showSlides = () => {
-//     const slidesToShow = 3;
-//     const offset = (currentIndex % totalSlides) * -100 / slidesToShow;
-//     const carouselInner = document.querySelector('.team__box');
-//     carouselInner.style.transform = `translateX(${offset}%)`;
+const showSlides = () => {
+    const slidesToShow = 3;
+    const offset = (currentIndex % totalSlides) * -100 / slidesToShow;
+    const carouselInner = document.querySelector('.team__box');
+    carouselInner.style.transform = `translateX(${offset}%)`;
 
-//     updateIndicator();
-// };
+    updateIndicator();
+};
 
-// const nextSlide = () => {
-//     currentIndex = (currentIndex + 1) % totalSlides; // Зацикливание индекса
-//     showSlides();
-// };
+const nextSlide = () => {
+    currentIndex = (currentIndex + 1) % totalSlides; // Зацикливание индекса
+    showSlides();
+};
 
-// showSlides(); // Отображаем первые слайды
+showSlides(); // Отображаем первые слайды
 
-// setInterval(nextSlide, 4000); // Переключение каждые 4 секунды
+setInterval(nextSlide, 4000); // Переключение каждые 4 секунды
 
-       
-        const sliders = document.querySelectorAll('.slide');
-        const totalSliders = sliders.length;
-        let curIndex = 0;
 
-        // Функция для отображения слайда
-        const showSlide = (index) => {
-            const offset = index * -100; // Сдвигаем на 100% ширины слайда
-            document.querySelector('.slides').style.transform = 'translateX(${offset}%)';
-        };
+const sliders = document.querySelectorAll('.slide');
+const totalSliders = sliders.length;
+let curIndex = 0;
 
-        // Обработчики событий для кнопок "вперед" и "назад"
-        document.getElementById('next-button').addEventListener('click', () => {
-            curIndex = (curIndex + 1) % totalSliders; // Переход к следующему слайду
-            showSlide(curIndex);
-        });
+// Функция для отображения слайда
+const showSlide = (index) => {
+    const offset = index * -100; // Сдвигаем на 100% ширины слайда
+    document.querySelector('.slides').style.transform = 'translateX(${offset}%)';
+};
 
-        document.getElementById('prev-button').addEventListener('click', () => {
-            curIndex = (curIndex - 1 + totalSliders) % totalSliders; // Переход к предыдущему слайду
-            showSlide(curIndex);
-        });
-           
-        showSlide();
-    
+// Обработчики событий для кнопок "вперед" и "назад"
+document.getElementById('next-button').addEventListener('click', () => {
+    curIndex = (curIndex + 1) % totalSliders; // Переход к следующему слайду
+    showSlide(curIndex);
+});
+
+document.getElementById('prev-button').addEventListener('click', () => {
+    curIndex = (curIndex - 1 + totalSliders) % totalSliders; // Переход к предыдущему слайду
+    showSlide(curIndex);
+});
+
+showSlide();
