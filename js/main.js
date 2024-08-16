95974fd9317e0204e2353901b738c2d1be704281
Torro15 <svetlgrish28@gmail.com>
2024-08-16 19:05:35 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,51 +1,29 @@
 
-const slides = document.querySelectorAll('.team__box-item');
-const totalSlides = slides.length;
-let currentIndex = 0;
+const images = document.querySelectorAll('.achievements__grid-mobile .slide');
+let curIndex = 0;
 
-const updateIndicator = () => {
-    const indicator = document.querySelector('.count');
-    indicator.innerText = `${currentIndex + 1}/6`;
-};
+function showImage(index) {
+    images[curIndex].classList.remove('active');
+    images[index].classList.add('active');
+    curIndex = index;
+}
 
-const showSlides = () => {
-    const slidesToShow = 3;
-    const offset = (currentIndex % totalSlides) * -100 / slidesToShow;
-    const carouselInner = document.querySelector('.team__box');
-    carouselInner.style.transform = `translateX(${offset}%)`;
+document
+    .querySelector('.achievements__grid-mobile.controlls')
+    .addEventListener('click', function (event) {
+        if (event.target.classList.contains('prev-button')) {
+            let index = curIndex - 1;
+            if (index < 0) {
+                index = images.length - 1;
+            }
+            showImage(index);
+        } else if (event.target.classList.contains('next-button')) {
+            let index = curIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            showImage(index);
+        }
+    });
 
-    updateIndicator();
-};
-
-const nextSlide = () => {
-    currentIndex = (currentIndex + 1) % totalSlides; // Зацикливание индекса
-    showSlides();
-};
-
-showSlides(); // Отображаем первые слайды
-
-setInterval(nextSlide, 4000); // Переключение каждые 4 секунды
-
-
-// const sliders = document.querySelectorAll('.slide');
-// const totalSliders = sliders.length;
-// let curIndex = 0;
-
-// // Функция для отображения слайда
-// const showSlide = (index) => {
-//     const offset = index * -100; // Сдвигаем на 100% ширины слайда
-//     document.querySelector('.slides').style.transform = 'translateX(${offset}%)';
-// };
-
-// // Обработчики событий для кнопок "вперед" и "назад"
-// document.getElementById('next-button').addEventListener('click', () => {
-//     curIndex = (curIndex + 1) % totalSliders; // Переход к следующему слайду
-//     showSlide(curIndex);
-// });
-
-// document.getElementById('prev-button').addEventListener('click', () => {
-//     curIndex = (curIndex - 1 + totalSliders) % totalSliders; // Переход к предыдущему слайду
-//     showSlide(curIndex);
-// });
-
-// showSlide();
+showImage(curIndex);
