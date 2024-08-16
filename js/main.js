b2b4c91da4a93901c4dda6afc1d34183cee8463f
Torro15 <svetlgrish28@gmail.com>
2024-08-16 18:20:21 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -27,25 +27,25 @@ showSlides(); // Отображаем первые слайды
 setInterval(nextSlide, 4000); // Переключение каждые 4 секунды
 
 
-const sliders = document.querySelectorAll('.slide');
-const totalSliders = sliders.length;
-let curIndex = 0;
+// const sliders = document.querySelectorAll('.slide');
+// const totalSliders = sliders.length;
+// let curIndex = 0;
 
-// Функция для отображения слайда
-const showSlide = (index) => {
-    const offset = index * -100; // Сдвигаем на 100% ширины слайда
-    document.querySelector('.slides').style.transform = 'translateX(${offset}%)';
-};
+// // Функция для отображения слайда
+// const showSlide = (index) => {
+//     const offset = index * -100; // Сдвигаем на 100% ширины слайда
+//     document.querySelector('.slides').style.transform = 'translateX(${offset}%)';
+// };
 
-// Обработчики событий для кнопок "вперед" и "назад"
-document.getElementById('next-button').addEventListener('click', () => {
-    curIndex = (curIndex + 1) % totalSliders; // Переход к следующему слайду
-    showSlide(curIndex);
-});
+// // Обработчики событий для кнопок "вперед" и "назад"
+// document.getElementById('next-button').addEventListener('click', () => {
+//     curIndex = (curIndex + 1) % totalSliders; // Переход к следующему слайду
+//     showSlide(curIndex);
+// });
 
-document.getElementById('prev-button').addEventListener('click', () => {
-    curIndex = (curIndex - 1 + totalSliders) % totalSliders; // Переход к предыдущему слайду
-    showSlide(curIndex);
-});
+// document.getElementById('prev-button').addEventListener('click', () => {
+//     curIndex = (curIndex - 1 + totalSliders) % totalSliders; // Переход к предыдущему слайду
+//     showSlide(curIndex);
+// });
 
-showSlide();
+// showSlide();
